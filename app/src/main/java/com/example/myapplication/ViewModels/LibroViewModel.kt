5505eacc92f3044468287ffb57fe56341a487548
f-molina <00300917@uca.edu.sv>
2019-05-23 17:46:39 -0600

    changed from 1 repo to 3 on viewmodel

--- app/src/main/java/com/example/myapplication/ViewModels/LibroViewModel.kt
+++ app/src/main/java/com/example/myapplication/ViewModels/LibroViewModel.kt
@@ -4,6 +4,9 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
+import com.example.myapplication.Repository.AutorRepository
+import com.example.myapplication.Repository.LibroRepository
+import com.example.myapplication.Repository.TagRepository
 import com.example.myapplication.database.AppDatabase
 import com.example.myapplication.database.DAO.AutorDAO
 import com.example.myapplication.database.DAO.LibroDAO
@@ -14,16 +17,20 @@ import kotlinx.coroutines.launch
 
 class LibroViewModel(application: Application): AndroidViewModel(application) {
     private val repository: LibroRepository
+    private val repositoryAutor: AutorRepository
+    private val repositoryTag: TagRepository
     val allBooks: LiveData<List<LibroEntity>>
     init {
         val booksDao: LibroDAO = AppDatabase.getDatabase(application, viewModelScope).libroDao()
         val autorDao: AutorDAO = AppDatabase.getDatabase(application, viewModelScope).autorDao()
         val tagDao: TagDAO = AppDatabase.getDatabase(application, viewModelScope).tagDao()
-        repository = LibroRepository(booksDao, autorDao, tagDao)
-        allBooks = repository.allBooks
+        repository = LibroRepository(booksDao)
+        repositoryAutor = AutorRepository(autorDao)
+        repositoryTag = TagRepository(tagDao)
+        allBooks = repository.allLibros
     }
 
-    fun insert(libroEntity: LibroEntity) =viewModelScope.launch(Dispatchers.IO) {
+    fun insert(libroEntity: LibroEntity) = viewModelScope.launch(Dispatchers.IO) {
         repository.insert(libroEntity)
     }
 }
