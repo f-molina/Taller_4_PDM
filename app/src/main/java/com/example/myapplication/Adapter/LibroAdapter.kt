8803defb73d55bbd5b3e8bef8aad5660f2641d1f
Valencia12 <valencia12@github.com>
2019-05-24 01:19:52 -0600

    agregando varias cosas, rv, adapte, cardviews, y uniendolo con el viewmodel

--- app/src/main/java/com/example/myapplication/Adapter/LibroAdapter.kt
+++ app/src/main/java/com/example/myapplication/Adapter/LibroAdapter.kt
@@ -1,31 +1,48 @@
 package com.example.myapplication.Adapter
 
 import android.content.Context
-import android.os.Parcel
-import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.myapplication.R
 import com.example.myapplication.database.Entities.LibroEntity
-import java.security.AccessControlContext
+import kotlinx.android.synthetic.main.cardviewlibro.view.*
 
- class LibroAdapter internal constructor(context: Context, val clickListenerBoton: (LibroEntity) -> Unit, val clickListenerViewHolder: (LibroEntity)-> Unit): RecyclerView.Adapter<LibroAdapter.ViewHolder>() {
+class LibroAdapter internal constructor(context: Context, val clickBoton: (LibroEntity) -> Unit, val clickListenerViewHolder: (LibroEntity)-> Unit): RecyclerView.Adapter<LibroAdapter.ViewHolder>() {
+
+
      private var libros: List<LibroEntity> = emptyList()
 
      override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LibroAdapter.ViewHolder {
          val view = LayoutInflater.from(parent.context).inflate(R.layout.cardviewlibro, parent, false)
-         return RecyclerView.ViewHolder(view)
+         return ViewHolder(view)
      }
 
      override fun getItemCount(): Int {
          TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
      }
 
-     override fun onBindViewHolder(holder: LibroAdapter.ViewHolder, position: Int) {
-         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+     override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(libros[position], clickBoton, clickListenerViewHolder)
+
+     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+         fun bind(libroentity: LibroEntity, clickBoton: (LibroEntity)->Unit, clickListenerViewHolder: (LibroEntity) -> Unit) = with(itemView){
+             Glide.with(itemView.context)
+                 .load(libroentity.caratula)
+                 .placeholder(R.drawable.ic_launcher_background)
+                 .into(Iv_book_portada)
+             tvlibronombre.text = libroentity.titulo
+
+             setFavorite.setOnClickListener {
+                 clickBoton(libroentity)
+
+             }
+             this.setOnClickListener(){
+                 clickListenerViewHolder(libroentity)
+             }
+
+         }
      }
 
  }
